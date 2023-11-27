0b0d6918486821d74ca8974ed7378a9e3601df59
Christian Lee <lee.christian64@outlook.com>
2023-11-27 10:24:19 -0800

    Init

---
+++ moneyline.py
@@ -0,0 +1,82 @@
+from fractions import Fraction
+from decimal import Decimal
+
+def as_american(odds):
+    if is_decimal(odds):
+        if odds > 2.0:
+            return "+" + str(round((odds - 1) * 100))
+        elif 1.01 <= odds <= 1.99:
+            return str(round(-100 / (odds - 1)))
+    elif is_fractional(odds):
+        f = Fraction(odds)
+        decimal = f.numerator / f.denominator
+        if decimal >= 1:
+            return "+" + str(round(decimal * 100))
+        else:
+            return "-" + str(round(-100 / decimal))
+    elif is_american(odds):
+        return odds
+    else:
+        raise ValueError("Can't convert {o} to american odds".format(o=odds))
+
+def as_decimal(odds):
+    if is_decimal(odds):
+        return odds
+    elif is_fractional(odds):
+        f = Fraction(odds)
+        return (f.numerator / f.denominator) + 1
+    elif is_american(odds):
+        if isinstance(odds, str) and len(odds) > 1:
+            sign = odds[0]
+            odds = int(odds[1:])
+            if sign == "+":
+                odds = (odds / 100) + 1
+                return round(odds, 2)
+            elif sign == "-":
+                odds = (100 / odds) + 1
+                return round(odds, 2)
+        else: 
+            raise()
+    else:
+        raise ValueError("Can't convert {o} to decimal odds".format(o=odds))
+
+def as_fraction(odds):
+    if is_decimal(odds):
+        f = Fraction(Decimal(str(odds)))
+        odds = f - 1
+        return str(odds)
+    elif is_fractional(odds):
+        return odds
+    elif is_american(odds):
+        decimal = as_decimal(odds)
+        return as_fraction(decimal)
+    else:
+        raise ValueError("Can't convert {o} to fractional odds".format(o=odds))
+
+def is_decimal(odds):
+    return isinstance(odds, float)
+
+def is_american(odds):
+    if isinstance(odds, str) and odds.strip():    
+        try:
+            sign = odds.strip()[0]
+            odds = odds.strip()[1:]
+            odds = int(odds)
+            if sign in ["+", "-"] and isinstance(odds, int):
+                return True
+        except:
+            print('Could not convert odds {o} to int'.format(o=odds))
+    return False
+
+def is_fractional(odds):
+    try:
+        if isinstance(odds, str) and odds.find('/') != -1:
+            Fraction(odds)
+            return True
+        return False
+    except:
+        print('is not fractional')
+        return False
+        
+
+        
---
+++ tests/test_moneyline.py
@@ -0,0 +1,107 @@
+import unittest
+from moneyline import *
+
+class TestMoneyline(unittest.TestCase):
+
+    def setUp(self):
+        self.o1 = 1.10
+        self.o2 = "+150"
+        self.o3 = "-110"
+        self.o4 = "200/100"
+        self.o5 = None
+        self.o6 = ""
+
+    def test_is_fractional(self):
+        result1 = is_fractional(self.o1)
+        result2 = is_fractional(self.o2)
+        result3 = is_fractional(self.o3)
+        result4 = is_fractional(self.o4)
+        result5 = is_fractional(self.o5)
+        result6 = is_fractional(self.o6)
+
+        self.assertEqual(result1, False)
+        self.assertEqual(result2, False)
+        self.assertEqual(result3, False)
+        self.assertEqual(result4, True)
+        self.assertEqual(result5, False)
+        self.assertEqual(result6, False)
+
+    def test_is_american(self):
+        result1 = is_american(self.o1)
+        result2 = is_american(self.o2)
+        result3 = is_american(self.o3)
+        result4 = is_american(self.o4)
+        result5 = is_american(self.o5)
+        result6 = is_american(self.o6)
+
+        self.assertEqual(result1, False)
+        self.assertEqual(result2, True)
+        self.assertEqual(result3, True)
+        self.assertEqual(result4, False)
+        self.assertEqual(result5, False)
+        self.assertEqual(result6, False)
+
+    def test_is_decimal(self):
+        result1 = is_decimal(self.o1)
+        result2 = is_decimal(self.o2)
+        result3 = is_decimal(self.o3)
+        result4 = is_decimal(self.o4)
+        result5 = is_decimal(self.o5)
+        result6 = is_decimal(self.o6)
+
+        self.assertEqual(result1, True)
+        self.assertEqual(result2, False)
+        self.assertEqual(result3, False)
+        self.assertEqual(result4, False)
+        self.assertEqual(result5, False)
+        self.assertEqual(result6, False)
+
+
+    def test_as_american(self):
+        result1 = as_american(self.o1)
+        result2 = as_american(self.o2)
+        result3 = as_american(self.o3)
+        result4 = as_american(self.o4)
+
+        self.assertEqual(result1, "-1000")
+        self.assertEqual(result2, "+150")
+        self.assertEqual(result3, "-110")
+        self.assertEqual(result4, "+200")
+       
+        with self.assertRaises(ValueError):
+            as_american(self.o5)
+
+        with self.assertRaises(ValueError):
+            as_american(self.o6)
+
+    def test_as_decimal(self):
+        result1 = as_decimal(self.o1)
+        result2 = as_decimal(self.o2)
+        result3 = as_decimal(self.o3)
+        result4 = as_decimal(self.o4)
+
+        self.assertEqual(result1, 1.10)
+        self.assertEqual(result2, 2.5)
+        self.assertEqual(result3, 1.91)
+        self.assertEqual(result4, 3.0)
+        
+        with self.assertRaises(ValueError):
+            as_decimal(self.o5)
+        with self.assertRaises(ValueError):
+            as_decimal(self.o6)
+
+    def test_as_fraction(self):
+        result1 = as_fraction(self.o1)
+        result2 = as_fraction(self.o2)
+        result3 = as_fraction(self.o3)
+        result4 = as_fraction(self.o4)
+
+        self.assertEqual(result1, "1/10")
+        self.assertEqual(result2, "3/2")
+        self.assertEqual(result3, "91/100")
+        self.assertEqual(result4, "200/100")
+
+        with self.assertRaises(ValueError):
+            as_fraction(self.o5)
+        with self.assertRaises(ValueError):
+            as_fraction(self.o6)
